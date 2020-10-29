eff312ea1858c9af4eed3720ef6d9cbcc7837179
Drillin Pierre <sio.drillin@gmail.com>
2020-10-29 11:02:15 +0100

    Add some tests for the automatic calculation of spatial related fields

--- pg_metadata/test/test_sql.py
+++ pg_metadata/test/test_sql.py
@@ -1,3 +1,4 @@
+from qgis.PyQt.QtCore import NULL
 from pg_metadata.test.base_database import DatabaseTestCase
 
 
@@ -36,3 +37,72 @@ class TestSql(DatabaseTestCase):
                 "SELECT pgmetadata.get_dataset_item_html_content('pgmetadata','lines','main')")
         )
         self.assertEqual("<p>Test title</p><b>Test title</b>", result[0][0])
+
+    def test_trigger_calculate_fields(self):
+        """ Test if fields are correctly calculated on a layer having
+        a geometry """
+
+        # Adding data
+        dataset_feature = {
+            'table_name': "'lines'",
+            'schema_name': "'pgmetadata'",
+            'title': "'Test title'",
+            'abstract': "'Test abstract.'",
+        }
+        self._insert(dataset_feature, 'dataset')
+
+        # Test insert
+        sql = "SELECT geometry_type, projection_authid, spatial_extent FROM pgmetadata.dataset"
+        result = self.connection.executeSql(sql)
+        self.assertEqual(['LINESTRING', 'EPSG:4326', '3.854, 3.897, 43.5786, 43.622'], result[0])
+
+        # Test update
+        sql = "UPDATE pgmetadata.dataset SET title = 'test lines title' WHERE table_name = 'lines'"
+        self.connection.executeSql(sql)
+        sql = "SELECT title, geometry_type, projection_authid, spatial_extent FROM pgmetadata.dataset"
+        result = self.connection.executeSql(sql)
+        self.assertEqual(
+            ['test lines title', 'LINESTRING', 'EPSG:4326', '3.854, 3.897, 43.5786, 43.622'],
+            result[0]
+        )
+
+    def test_trigger_calculate_fields_when_removing_geom(self):
+        """ Test the trigger on an existing table where we remove the geometry. """
+
+        # Adding data
+        dataset_feature = {
+            'table_name': "'lines'",
+            'schema_name': "'pgmetadata'",
+            'title': "'Test title'",
+            'abstract': "'Test abstract.'",
+        }
+        self._insert(dataset_feature, 'dataset')
+
+        # Test with removing the geom column
+        sql = "ALTER TABLE pgmetadata.lines DROP COLUMN geom"
+        self.connection.executeSql(sql)
+        sql = "UPDATE pgmetadata.dataset SET title = 'test after drop geom column'"
+        self.connection.executeSql(sql)
+        sql = "SELECT title, geometry_type, projection_authid, spatial_extent, geom FROM pgmetadata.dataset"
+        result = self.connection.executeSql(sql)
+        self.assertEqual(['test after drop geom column', NULL, NULL, NULL, NULL], result[0])
+
+    def test_trigger_calculate_fields_without_geom(self):
+        """ Test compute fields on a geometry less table. """
+
+        # Test with new table whithout geom column
+        sql = "CREATE TABLE pgmetadata.testwithoutgeom (id serial, name text)"
+        self.connection.executeSql(sql)
+        dataset_feature = {
+            'table_name': "'testwithoutgeom'",
+            'schema_name': "'pgmetadata'",
+            'title': "'Test title'",
+            'abstract': "'Test abstract.'",
+        }
+        self._insert(dataset_feature, 'dataset')
+        sql = (
+            "SELECT geometry_type, projection_authid, spatial_extent, geom FROM pgmetadata.dataset WHERE"
+            " table_name = 'testwithoutgeom'"
+        )
+        result = self.connection.executeSql(sql)
+        self.assertEqual([NULL, NULL, NULL, NULL], result[0])
