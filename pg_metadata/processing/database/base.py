b970e766bf0a55d53b47be22a99d768676d3a55d
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-11-02 18:33:07 +0100

    Execute a vacuum on all tables after working on them

--- pg_metadata/processing/database/base.py
+++ pg_metadata/processing/database/base.py
@@ -2,6 +2,12 @@ __copyright__ = 'Copyright 2020, 3Liz'
 __license__ = 'GPL version 3'
 __email__ = 'info@3liz.org'
 
+from qgis.core import (
+    QgsAbstractDatabaseProviderConnection,
+    QgsProcessingFeedback,
+    QgsProviderConnectionException,
+)
+
 from pg_metadata.qgis_plugin_tools.tools.algorithm_processing import (
     BaseProcessingAlgorithm,
 )
@@ -14,4 +20,17 @@ class BaseDatabaseAlgorithm(BaseProcessingAlgorithm):
         return tr('Database')
 
     def groupId(self):
-        return 'databse'
+        return 'database'
+
+    @staticmethod
+    def vacuum_all_tables(
+            connection: QgsAbstractDatabaseProviderConnection, feedback: QgsProcessingFeedback):
+        """ Execute a vacuum to recompute the feature count. """
+        for table in connection.tables('pgmetadata'):
+
+            sql = 'VACUUM ANALYSE {}.{};'.format('pgmetadata', table.tableName())
+            feedback.pushDebugInfo(sql)
+            try:
+                connection.executeSql(sql)
+            except QgsProviderConnectionException as e:
+                feedback.reportError(str(e))
