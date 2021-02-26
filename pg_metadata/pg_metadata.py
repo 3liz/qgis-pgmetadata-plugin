6696d32056ac030eabe203e26ae1ffe15de2d9c2
Etienne Trimaille <gustrimaille@yahoo.fr>
2021-02-26 08:46:58 +0700

    UX - Review actions in menus

--- pg_metadata/pg_metadata.py
+++ pg_metadata/pg_metadata.py
@@ -1,4 +1,4 @@
-__copyright__ = "Copyright 2020, 3Liz"
+__copyright__ = "Copyright 2021, 3Liz"
 __license__ = "GPL version 3"
 __email__ = "info@3liz.org"
 
@@ -7,6 +7,7 @@ from qgis.core import QgsApplication
 from qgis.PyQt.QtCore import QCoreApplication, Qt, QTranslator, QUrl
 from qgis.PyQt.QtGui import QDesktopServices, QIcon
 from qgis.PyQt.QtWidgets import QAction
+from qgis.utils import iface
 
 from pg_metadata.dock import PgMetadataDock
 from pg_metadata.locator import LocatorFilter
@@ -20,8 +21,8 @@ from pg_metadata.qgis_plugin_tools.tools.resources import (
 
 
 class PgMetadata:
-    def __init__(self, iface):
-        self.iface = iface
+    def __init__(self):
+        """ Constructor. """
         self.dock = None
         self.provider = None
         self.locator_filter = None
@@ -39,29 +40,34 @@ class PgMetadata:
             QCoreApplication.installTranslator(self.translator)
 
     def initProcessing(self):
+        """ Add the QGIS Processing provider. """
         if not self.provider:
             self.provider = PgMetadataProvider()
             QgsApplication.processingRegistry().addProvider(self.provider)
 
     def initGui(self):
+        """ Build the plugin GUI. """
         self.initProcessing()
+
+        icon = QIcon(resources_path('icons', 'icon.png'))
+
+        # Open the online help
+        self.help_action = QAction(icon, 'PgMetadata', iface.mainWindow())
+        iface.pluginHelpMenu().addAction(self.help_action)
+        self.help_action.triggered.connect(self.open_help)
 
         if not self.dock:
             self.dock = PgMetadataDock()
-            self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
+            iface.addDockWidget(Qt.RightDockWidgetArea, self.dock)
+
+            # Open/close the dock from plugin menu
+            self.dock_action = QAction(icon, 'PgMetadata', iface.mainWindow())
+            iface.pluginMenu().addAction(self.dock_action)
+            self.dock_action.triggered.connect(self.open_dock)
 
         if not self.locator_filter:
-            self.locator_filter = LocatorFilter(self.iface)
-            self.iface.registerLocatorFilter(self.locator_filter)
-
-        icon = QIcon(resources_path('icons', 'icon.png'))
-        self.dock_action = QAction(icon, 'PgMetadata', self.iface.mainWindow())
-        self.iface.pluginMenu().addAction(self.dock_action)
-        self.dock_action.triggered.connect(self.open_dock)
-
-        self.help_action = QAction(icon, 'PgMetadata', self.iface.mainWindow())
-        self.iface.pluginHelpMenu().addAction(self.dock_action)
-        self.dock_action.triggered.connect(self.open_help)
+            self.locator_filter = LocatorFilter(iface)
+            iface.registerLocatorFilter(self.locator_filter)
 
     @staticmethod
     def open_help():
@@ -74,8 +80,9 @@ class PgMetadata:
         self.dock.raise_()
 
     def unload(self):
+        """ Unload the plugin. """
         if self.dock:
-            self.iface.removeDockWidget(self.dock)
+            iface.removeDockWidget(self.dock)
             self.dock.deleteLater()
 
         if self.provider:
@@ -83,15 +90,15 @@ class PgMetadata:
             del self.provider
 
         if self.locator_filter:
-            self.iface.deregisterLocatorFilter(self.locator_filter)
+            iface.deregisterLocatorFilter(self.locator_filter)
             del self.locator_filter
 
         if self.dock_action:
-            self.iface.pluginMenu().removeAction(self.dock_action)
+            iface.pluginMenu().removeAction(self.dock_action)
             del self.dock_action
 
         if self.help_action:
-            self.iface.pluginHelpMenu().removeAction(self.help_action)
+            iface.pluginHelpMenu().removeAction(self.help_action)
             del self.help_action
 
     @staticmethod
