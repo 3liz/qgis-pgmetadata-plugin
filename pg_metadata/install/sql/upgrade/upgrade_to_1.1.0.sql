1f3a8fdb1fae0bea1a60765ead42545287478a2b
Thomas Schüttenberg <thomas@qgis.de>
2021-07-06 12:15:31 +0200

    unittest error, fix typo

--- pg_metadata/install/sql/upgrade/upgrade_to_1.1.0.sql
+++ pg_metadata/install/sql/upgrade/upgrade_to_1.1.0.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE VIEW pgmetadata.v_orphan_tables
     tables.table_name::text AS tablename
    FROM information_schema.tables
   WHERE NOT (concat(tables.table_schema, '.', tables.table_name) IN ( SELECT concat(dataset.schema_name, '.', dataset.table_name) AS concat
-           FROM pgmetadata.dataset)) AND (tables.table_schema::name <> ALL (ARRAY['pg_catalog'::name, 'information_schema'::name, 'tiger'::name, 'topology'::name]))
+           FROM pgmetadata.dataset)) AND (tables.table_schema::name <> ALL (ARRAY['pg_catalog'::name, 'information_schema'::name]))
   ORDER BY (tables.table_schema::text), (tables.table_name::text);
 
 -- v_orphan_dataset_items add support for views
@@ -31,6 +31,6 @@ CREATE OR REPLACE VIEW pgmetadata.v_valid_dataset
      LEFT JOIN information_schema.tables t ON d.schema_name = t.table_schema::text AND d.table_name = t.table_name::text
   WHERE t.table_name IS NOT NULL
   ORDER BY d.schema_name, d.table_name;
- 
- 
- COMMIT;
+
+
+COMMIT;
