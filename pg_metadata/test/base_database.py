eee36e6e61820cdb2f7b52bd69d7ee27486a0dda
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-09-05 18:13:47 +0200

    Python - Remove unused line from Python files

--- pg_metadata/test/base_database.py
+++ pg_metadata/test/base_database.py
@@ -26,7 +26,6 @@ from pg_metadata.test.base import BaseTestProcessing
 __copyright__ = "Copyright 2020, 3Liz"
 __license__ = "GPL version 3"
 __email__ = "info@3liz.org"
-__revision__ = "$Format:%H$"
 
 SCHEMA = "pgmetadata"
 
--- pg_metadata/test/test_database_structure.py
+++ pg_metadata/test/test_database_structure.py
@@ -23,7 +23,6 @@ from pg_metadata.qgis_plugin_tools.tools.logger_processing import (
 __copyright__ = "Copyright 2020, 3Liz"
 __license__ = "GPL version 3"
 __email__ = "info@3liz.org"
-__revision__ = "$Format:%H$"
 
 VERSION = '0.0.1'
 SCHEMA = "pgmetadata"
